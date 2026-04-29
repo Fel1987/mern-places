d9c69394688388ad5c06dc248207c79cca816d3f
Felipe A Walton A <felwal1987@gmail.com>
2026-04-28 21:37:43 -0600

    added Places page and filter for user places

---
+++ src/places/pages/UserPlaces.jsx
@@ -0,0 +1,34 @@
+import PlaceList from "../components/PlaceList";
+import { useParams } from "react-router";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "Not much to see...",
+    imageUrl:
+      "https://www.exp1.com/wp-content/uploads/sites/7/2020/07/empire_state_building_50_1_50-1024x683.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: { lat: 40.7484405, lng: -73.9856644 },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "Not much to see...",
+    imageUrl:
+      "https://www.exp1.com/wp-content/uploads/sites/7/2020/07/empire_state_building_50_1_50-1024x683.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: { lat: 40.7484405, lng: -73.9856644 },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = (props) => {
+  const { userId } = useParams();
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  console.log(loadedPlaces);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
